c44fe1645ec14b28f47084fd05cfa6d1cd1b0f18
n76219848 <nurlanguliyev@std.iyte.edu.tr>
2020-10-26 09:12:36 +0000

    Example 3 in a best shape

--- lab3/example3.py
+++ lab3/example3.py
@@ -3,7 +3,7 @@ num=int(input("Enter the number of lectures"))
 
 if gpa<2.0 and num<47:
   print("Not enough number of lectures and lessons")
-elif gpa<2.0 and num>47:
+elif gpa<2.0 and num>=47:
   print("Not enough gpa !")
 elif gpa>=2.0 and num<47:
   print("Not enough number of lectures")
