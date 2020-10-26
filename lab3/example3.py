3a28625746cb463ff123415ae8eb9dfdc0b2db67
n76219848 <nurlanguliyev@std.iyte.edu.tr>
2020-10-26 08:56:44 +0000

    Third example of lab3.

--- lab3/example3.py
+++ lab3/example3.py
@@ -5,7 +5,7 @@ if gpa<2.0 and num<47:
   print("Not enough number of lectures and lessons")
 elif gpa<2.0 and num>47:
   print("Not enough gpa !")
-elif gpa>2.0 and num<47:
+elif gpa>=2.0 and num<47:
   print("Not enough number of lectures")
 else:
   print("Graduated")
