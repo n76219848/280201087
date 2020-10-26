754dad6187409054f16cdd81220ad0a076f7fe75
n76219848 <nurlanguliyev@std.iyte.edu.tr>
2020-10-26 08:43:25 +0000

    second example of lab3

---
+++ lab3/example2.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a<b and a<c:
+  print(a ,'is the minimum value')
+elif b<a and b<c:
+  print(b, 'is the minimum value')
+else:
+  print(c, "is the minimum value") 
+  
