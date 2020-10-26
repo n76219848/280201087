bed5c15b46e473bd0a2a1c1843e42d67181e1423
n76219848 <nurlanguliyev@std.iyte.edu.tr>
2020-10-26 08:50:10 +0000

    third example of lab3

---
+++ lab3/example3.py
@@ -0,0 +1,11 @@
+gpa=float(input("Enter your GPA"))
+num=int(input("Enter the number of lectures"))
+
+if gpa<2.0 and num<47:
+  print("Not enough number of lectures and lessons")
+elif gpa<2.0 and num>47:
+  print("Not enough gpa !")
+elif gpa>2.0 and num<47:
+  print("Not enough number of lectures")
+else:
+  print("Graduated")
