64088889b25055d870f4ecf6efb3a4f9bf470cf6
n76219848 <nurlanguliyev@std.iyte.edu.tr>
2020-12-27 19:40:54 +0000

    Example 4 of lab8

---
+++ lab8/example4.py
@@ -0,0 +1,40 @@
+def binary_to_dec(b):
+  
+  d = 0
+  
+  b_rev = str(b)[::-1] # reverse
+  
+  for i in range(len(b_rev)):
+  
+    d += (2*i)
+    
+    int(b_rev[i])
+  
+  return(d)
+
+
+
+def dec_to_binary(d):
+  
+  b = ""
+  
+  while d > 0:
+    
+    b += str(d%2)
+    
+    d //= 2
+
+  return(b[::-1])
+
+def main_():
+  
+  b = "10010"
+  
+  print(binary_to_dec(b))
+  
+  d = 18
+
+  print(dec_to_binary(d))
+
+
+main_()
